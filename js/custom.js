fe38e5307b9b74ff70f8d425de7d8e47addc36b4
erwindacua <erwindacua@gmail.com>
2016-04-11 09:10:45 +0800

    Default Codes
    Default Codes of WHS

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function preloader() {
+	$(window).load(function() {
+		$(".animationload").delay(600).fadeOut("slow");
+	});
+}
+
+
+function setCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
